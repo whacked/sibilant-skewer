682ca1b64fe23fb05c034d022999498482870db8
natto <natto@natto-tpy.none>
2016-05-01 11:59:25 -0400

    update compile logic to handle names/unnamed files

--- sibilant-skewer.el
+++ sibilant-skewer.el
@@ -6,6 +6,34 @@
 
 (setq sibilant-program "~/node_modules/bin/sibilant")
 
+(setq-local inferior-lisp-program sibilant-program)
+
+(defun sibilant-mode/compile ()
+  "Invoke the Sibilant compiler for the current buffer."
+  (interactive)
+  (if (string-match "\\.sib\\(ilant\\)?$"
+                    buffer-file-name)
+      ;; if we are working with a sibilant file,
+      ;; output the js file in the same directory PLUS source map
+      (progn
+        (save-buffer)
+        (call-process sibilant-program
+                      nil nil nil
+                      "--file" buffer-file-name
+                      "--sourcemap"
+                      "--output" default-directory)
+        (message "Compiled with sourcemap to %s" default-directory))
+    ;; else we are working with some arbitrary buffer
+    (let ((output-name (format "%s.js" (file-name-sans-extension (file-relative-name buffer-file-name)))))
+      (shell-command-on-region
+       (point-min)
+       (point-max)
+       (concat sibilant-program " --input")
+       output-name)
+      (with-current-buffer (get-buffer output-name)
+        (save-buffer))
+      (message "Compiled to %s" output-name))))
+
 (defun sibilant-skewer-send-region (beg end)
   (let ((selected (buffer-substring beg end)))
     (with-temp-buffer
